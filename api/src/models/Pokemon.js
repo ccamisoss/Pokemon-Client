e3481c6f3a11aec25cd6b26b02354fbb01740237
Camila <camilaluciasosa@hotmail.com>
2021-11-19 14:47:45 -0300

    models done

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,32 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    vida: {
+      type: DataTypes.INTEGER
+    },
+    fuerza: {
+      type: DataTypes.INTEGER
+    },
+    defensa: {
+      type: DataTypes.INTEGER
+    },
+    velocidad: {
+      type: DataTypes.INTEGER
+    },
+    altura: {
+      type: DataTypes.INTEGER
+    },
+    peso: {
+      type: DataTypes.INTEGER
+    }
   });
 };
