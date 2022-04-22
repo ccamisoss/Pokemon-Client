7c6e5980efd14b71d42de8cd7c27ed6fe5277316
Camila <camilaluciasosa@hotmail.com>
2022-04-21 21:03:39 -0300

    Validated form

--- client/src/components/PokemonCreate.jsx
+++ client/src/components/PokemonCreate.jsx
@@ -3,12 +3,10 @@ import styles from "../styles/PokemonCreate.module.css";
 import { useEffect, useState } from "react";
 import { createPoke } from "../actions";
 import { useDispatch, useSelector } from "react-redux";
-import { getTypes } from "../actions/index"
+import { getTypes } from "../actions/index";
 
 export function PokemonCreate() {
-  const dispatch = useDispatch();
-  const types = useSelector((state) => state.tipos);
-  const [p, setPokemon] = useState({
+  let initialState = {
     name: "",
     height: "",
     weight: "",
@@ -16,38 +14,108 @@ export function PokemonCreate() {
     attack: "",
     defense: "",
     speed: "",
-    type: []
+    type: [],
+  };
+
+  const dispatch = useDispatch();
+  const types = useSelector((state) => state.tipos);
+  const [p, setPokemon] = useState(initialState);
+  const [error, setError] = useState({
+    nameError: "",
+    statError: "",
   });
 
   useEffect(() => {
-    dispatch(getTypes())
-  }, [dispatch])
+    dispatch(getTypes());
+  }, [dispatch]);
 
   const handleChange = (e) => {
-      setPokemon({
-        ...p,
-        [e.target.name]: e.target.value,
+    setPokemon({
+      ...p,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const validate = () => {
+    let nameError = "";
+    let statError = "";
+
+    if (/[a-zA-Z]/g.test(p.name) === false) {
+      nameError = "El nombre debe contener letras";
+    }
+    if (nameError) {
+      setError({
+        ...error,
+        nameError: nameError,
       });
+      return false;
+    }
+
+    if (
+      typeof parseInt(p.height) !== "number" ||
+      typeof parseInt(p.attack) !== "number" ||
+      typeof parseInt(p.defense) !== "number" ||
+      typeof parseInt(p.hp) !== "number" ||
+      typeof parseInt(p.speed) !== "number" ||
+      typeof parseInt(p.weight) !== "number"
+    ) {
+      statError = "Todas las estadisticas deben ser numeros del 0 al 1000";
+    } else if (
+      p.height > 1000 ||
+      p.attack > 1000 ||
+      p.defense > 1000 ||
+      p.hp > 1000 ||
+      p.speed > 1000 ||
+      p.weight > 1000
+    ) {
+      statError = "Todas las estadisticas deben ser numeros del 0 al 1000";
+    } else if (
+      p.height < 0 ||
+      p.attack < 0 ||
+      p.defense < 0 ||
+      p.hp < 0 ||
+      p.speed < 0 ||
+      p.weight < 0
+    ) {
+      statError = "Todas las estadisticas deben ser numeros del 0 al 1000";
+    }
+
+    if (statError) {
+      setError({
+        ...error,
+        statError: statError,
+      });
+      return false;
+    }
+
+    return true;
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(
-      createPoke({
-        pokemon: {
-          name: p.name,
-          height: parseInt(p.height),
-          weight: parseInt(p.weight),
-          hp: parseInt(p.hp),
-          attack: parseInt(p.attack),
-          defense: parseInt(p.defense),
-          speed: parseInt(p.speed),
-        },
-        tipos: p.type,
-      })
-    );
-    window.location.reload()
-    alert("El pokemon ha sido creado con exito!");
+    const isValid = validate();
+    if (isValid) {
+      dispatch(
+        createPoke({
+          pokemon: {
+            name: p.name,
+            height: parseInt(p.height),
+            weight: parseInt(p.weight),
+            hp: parseInt(p.hp),
+            attack: parseInt(p.attack),
+            defense: parseInt(p.defense),
+            speed: parseInt(p.speed),
+          },
+          tipos: p.type,
+        })
+      );
+      setError({
+        nameError: "",
+        statError: "",
+      });
+      setPokemon(initialState);
+      alert("El pokemon ha sido creado con exito!");
+    }
   };
 
   const handleSelect = (e) => {
@@ -69,29 +137,78 @@ export function PokemonCreate() {
       <NavBar />
       <form onSubmit={handleSubmit} className={styles.contenedor}>
         <label>Nombre del Pokemon:</label>
-        <input name="name" value={p.name} onChange={handleChange} />
+        <input
+          className={styles.input}
+          required={true}
+          name="name"
+          value={p.name}
+          onChange={handleChange}
+        />
+        <h1 className={styles.h1}>{error.nameError}</h1>
         <label>Altura:</label>
-        <input name="height" value={p.height} onChange={handleChange} />
+        <input
+          className={styles.input}
+          required={true}
+          name="height"
+          value={p.height}
+          onChange={handleChange}
+        />
         <label>Peso:</label>
-        <input name="weight" value={p.weight} onChange={handleChange} />
+        <input
+          className={styles.input}
+          required={true}
+          name="weight"
+          value={p.weight}
+          onChange={handleChange}
+        />
         <label>Vida:</label>
-        <input name="hp" value={p.hp} onChange={handleChange} />
+        <input
+          className={styles.input}
+          required={true}
+          name="hp"
+          value={p.hp}
+          onChange={handleChange}
+        />
         <label>Ataque:</label>
-        <input name="attack" value={p.attack} onChange={handleChange} />
+        <input
+          className={styles.input}
+          required={true}
+          name="attack"
+          value={p.attack}
+          onChange={handleChange}
+        />
         <label>Defensa:</label>
-        <input name="defense" value={p.defense} onChange={handleChange} />
+        <input
+          className={styles.input}
+          required={true}
+          name="defense"
+          value={p.defense}
+          onChange={handleChange}
+        />
         <label>Velocidad:</label>
-        <input name="speed" value={p.speed} onChange={handleChange} />
-        <label>Tipo de Pokemon:</label>
+        <input
+          className={styles.input}
+          required={true}
+          name="speed"
+          value={p.speed}
+          onChange={handleChange}
+        />
+        <h1 className={styles.h1}>{error.statError}</h1>
+        <label className={styles.label}>Tipo de Pokemon:</label>
         <div className={styles.select}>
-          {types?.map((t) => (
-            <label>
-              <input type="checkbox" value={t.name} onClick={handleSelect} />
+          {types?.map((t, i) => (
+            <label key={i}>
+              <input
+                className={styles.types}
+                type="checkbox"
+                value={t.name}
+                onClick={handleSelect}
+              />
               {t.name.charAt(0).toUpperCase() + t.name?.slice(1)}
             </label>
           ))}
         </div>
-          <button >Crear Pokemon</button> 
+        <button>Crear Pokemon</button>
       </form>
     </div>
   );
