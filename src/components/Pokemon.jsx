0e082eb6005740f10783c2e216b59869da2360c9
Lucy <camilaluciasosa@hotmail.com>
2023-12-03 12:49:03 -0300

    feat: create hook to capitalize first letter of a string

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,16 +1,17 @@
 import { NavLink } from "react-router-dom";
 import styles from "../styles/Pokemon.module.css";
+import { capitalizeFirstLetter } from "../utils"
 
 export function Pokemon({ name, image, types, id }) {
   return (
     <NavLink className={styles.nav} to={`/pokemon/${id}`}>
       <div className={styles.contenedor}>
-        <h3>{name}</h3>
+        <h3>{capitalizeFirstLetter(name)}</h3>
         <img className={styles.pokeImg} src={image} alt="" />
         <div>
           <h5>Tipos: </h5>
           {types?.map((t,i) => (
-            <h5 key={i}>{t}. </h5>
+            <h5 key={i}>{capitalizeFirstLetter(t)}. </h5>
           ))}
         </div>
       </div>
